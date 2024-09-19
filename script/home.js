9d1d3d3d8915dc91b82f1144fa61b9b60d685626
Himu004 <himu.chowdhury01@gmail.com>
2024-09-19 14:03:16 +0600

    Add Money & Cash Out Form Implemented

--- script/home.js
+++ script/home.js
@@ -1,3 +1,17 @@
+// Log out BTN
 document.getElementById("logout").addEventListener("click", function () {
     window.location.href = "/index.html";
+});
+
+// Add Money BTN
+document.getElementById("add-money-btn").addEventListener("click", function () {
+    document.getElementById("add-money-form").classList.remove("hidden");
+    document.getElementById("cash-out-form").classList.add("hidden");
+    
+});
+
+// Cash Out BTN
+document.getElementById("cash-out-btn").addEventListener("click", function () {
+    document.getElementById("add-money-form").classList.add("hidden");
+    document.getElementById("cash-out-form").classList.remove("hidden");
 });
